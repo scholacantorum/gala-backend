23c9d2e412826d3bba0cbbd4ba249e230268ad24
Steve Roth <steve.roth@hpe.com>
2019-05-03 17:42:51 -0700

    Rewrite the receipts layout per Ann's feedback.

--- guest/receipts.go
+++ guest/receipts.go
@@ -33,7 +33,15 @@ func serveReceipts(w *request.ResponseWriter, r *request.Request) {
   border-top: 1px solid black;
   padding: 24px 12px;
 }
+.valueNote {
+  font-style: italic;
+  padding-left: 2em;
+  padding-bottom: 12pt;
+}
 @media print {
+  html, body {
+    margin: 0;
+  }
   .printthis {
     display: none;
   }
@@ -71,28 +79,31 @@ func serveReceipts(w *request.ResponseWriter, r *request.Request) {
 
 func emitReceipt(w *request.ResponseWriter, r *request.Request, payer *model.Guest, purchases []*model.Purchase) {
 	type purchase struct {
-		Item   string
-		Bidder string
-		Amount int
-		Value  int
-		Date   string
-		Method string
+		ItemID   db.ID
+		Item     string
+		Amount   int
+		Value    int
+		Note     string
+		Date     string
+		Method   string
+		Quantity int
 	}
 	var receiptData struct {
-		Payer           string
-		Date            string
-		Method          string
-		MultipleBidders bool
-		HasValues       bool
-		ShowTotalValue  bool
-		TotalValue      int
-		TotalAmount     int
-		Purchases       []purchase
-		Pledges         []purchase
+		Payer                string
+		PurchaseTypes        string
+		ShowTotalValue       bool
+		TotalValue           int
+		TotalAmount          int
+		Purchases            []*purchase
+		ShowRegistrationNote bool
+		ShowPurchaseNote     bool
+		ShowDonationNote     bool
 	}
 	var (
-		bidder           string
-		multiplePayments bool
+		purchasesCount int
+		donations      int
+		pledges        int
+		types          []string
 	)
 
 	// Fill in the template data.
@@ -101,156 +112,149 @@ func emitReceipt(w *request.ResponseWriter, r *request.Request, payer *model.Gue
 	for _, p := range purchases {
 		var item = model.FetchItem(r.Tx, p.ItemID)
 		var purchase = purchase{
-			Item:   item.Name,
-			Bidder: model.FetchGuest(r.Tx, p.GuestID).Name,
-			Amount: p.Amount / 100,
-			Value:  item.Value / 100,
+			ItemID:   p.ItemID,
+			Item:     item.Name,
+			Amount:   p.Amount / 100,
+			Value:    item.Value / 100,
+			Quantity: 1,
 		}
-		if bidder == "" {
-			bidder = purchase.Bidder
-		} else if bidder != purchase.Bidder {
-			receiptData.MultipleBidders = true
+		if p.ItemID == 1 {
+			purchase.Note = "*"
+			receiptData.ShowRegistrationNote = true
+		} else if item.Value != 0 {
+			purchase.Note = "†"
+			receiptData.ShowPurchaseNote = true
+		} else {
+			purchase.Note = "§"
+			receiptData.ShowDonationNote = true
 		}
-		if item.Value != 0 {
-			receiptData.HasValues = true
+		if item.Value > p.Amount {
+			receiptData.ShowTotalValue = false
 		}
 		if p.PaymentTimestamp == "" {
-			receiptData.Pledges = append(receiptData.Pledges, purchase)
+			purchase.Amount = 0
+			pledges++
 		} else {
 			purchase.Date = p.PaymentTimestamp[0:10]
 			purchase.Method = p.PaymentDescription
-			if p.ScholaOrder != 0 {
-				purchase.Method = fmt.Sprintf("%s (Schola order #%d)", purchase.Method, p.ScholaOrder)
+			if item.Value != 0 {
+				purchasesCount++
+			} else {
+				donations++
 			}
-			receiptData.Purchases = append(receiptData.Purchases, purchase)
-			if item.Value > p.Amount {
-				receiptData.ShowTotalValue = false
-			}
-			receiptData.TotalAmount += purchase.Amount
-			receiptData.TotalValue += purchase.Value
 		}
+		receiptData.Purchases = append(receiptData.Purchases, &purchase)
+		receiptData.TotalAmount += purchase.Amount
+		receiptData.TotalValue += purchase.Value
 	}
-	for i := len(receiptData.Purchases) - 2; i >= 0; i-- {
-		a := receiptData.Purchases[i]
-		b := receiptData.Purchases[i+1]
-		if a.Date == b.Date && a.Method == b.Method {
-			receiptData.Purchases[i+1].Date = ""
-			receiptData.Purchases[i+1].Method = ""
+	// Combine multiple registrations with the same payment into a single
+	// line item.
+	for i := 1; i < len(receiptData.Purchases); {
+		a := receiptData.Purchases[i-1]
+		b := receiptData.Purchases[i]
+		if a.ItemID == 1 && b.ItemID == 1 && a.Date == b.Date && a.Method == b.Method {
+			a.Amount += b.Amount
+			a.Value += b.Value
+			a.Quantity += b.Quantity
+			a.Item = fmt.Sprintf("Registrations (%d)", a.Quantity)
+			receiptData.Purchases = append(receiptData.Purchases[:i], receiptData.Purchases[i+1:]...)
+			// and don't increment i
 		} else {
-			multiplePayments = true
+			i++
 		}
 	}
-	if len(receiptData.Purchases) > 0 && !multiplePayments {
-		p := receiptData.Purchases[0]
-		receiptData.Date = p.Date
-		receiptData.Method = p.Method
-		receiptData.Purchases[0].Date = ""
-		receiptData.Purchases[0].Method = ""
+	// Generate the English phrase to describe what they did.
+	if purchasesCount > 1 {
+		types = append(types, "purchases")
+	}
+	if purchasesCount == 1 {
+		types = append(types, "purchase")
+	}
+	if donations > 1 {
+		types = append(types, "donations")
+	}
+	if donations == 1 {
+		types = append(types, "donation")
+	}
+	if pledges > 1 {
+		types = append(types, "pledges")
+	}
+	if pledges == 1 {
+		types = append(types, "pledge")
+	}
+	switch len(types) {
+	case 1:
+		receiptData.PurchaseTypes = types[0]
+	case 2:
+		receiptData.PurchaseTypes = fmt.Sprintf("%s and %s", types[0], types[1])
+	case 3:
+		receiptData.PurchaseTypes = fmt.Sprintf("%s, %s, and %s", types[0], types[1], types[2])
+	}
+	// Suppress the total line if there's only one item.
+	if purchasesCount+donations+pledges < 2 {
+		receiptData.TotalAmount = 0
 	}
 
 	// Render the email template.
 	payerTemplate.Execute(w, &receiptData)
 }
 
-var paidTableTemplate = `
-<table>
+var payerTemplate = template.Must(template.New("payer").Parse(`
+<div class="receipt">
+<p>Schola Cantorum confirms the following {{ .PurchaseTypes }} from <b>{{ .Payer }}</b>:</p>
+<table style="margin-bottom:12pt">
   <thead>
     <tr>
-      <th style="text-align:left">Item</th>
-      {{ if .MultipleBidders }}
-	<th style="text-align:left;padding-left:1em">Bidder</th>
-      {{ end }}
-      <th style="text-align:right;padding-left:1em">Amount Paid</th>
-      {{ if .HasValues }}
-        <th style="text-align:right;padding-left:1em">Estimated Value</th>
-      {{ end }}
+      <th style="text-align:left;vertical-align:bottom">Item</th>
+      <th style="text-align:right;vertical-align:bottom;padding-left:1em">Estimated<br>Value<br>Received</th>
+      <th style="text-align:right;vertical-align:bottom;padding-left:1em">Amount<br>Paid</th>
+      <th style="text-align:left;vertical-align:bottom;padding-left:1em">Payment<br>Date</th>
+      <th style="text-align:left;vertical-align:bottom;padding-left:1em">Payment<br>Method</th>
     </tr>
   </thead>
   <tbody>
     {{ range .Purchases }}
-      {{ if .Date }}
-      <tr>
-        <td colspan="4" style="font-style:italic;color:#444">Paid on {{ .Date }} by {{ .Method }}:</td>
-      </tr>
-      {{ end }}
       <tr>
         <td>{{ .Item }}</td>
-	{{ if $.MultipleBidders }}
-	  <td style="padding-left:1em">{{ .Bidder }}</td>
-	{{ end }}
-	<td style="text-align:right">{{ .Amount }}</td>
-	{{ if $.HasValues }}
-	  <td style="text-align:right">{{ .Value }}</td>
-	{{ end }}
+        <td style="text-align:right">{{ .Value }}<sup>{{ .Note }}</sup></td>
+        {{ if .Amount }}
+          <td style="text-align:right">{{ .Amount }}</td>
+          <td style="padding-left:1em;white-space:nowrap">{{ .Date }}</td>
+          <td style="padding-left:1em;white-space:nowrap">{{ .Method }}</td>
+        {{ else }}
+          <td colspan="3" style="padding-left:1em"><i>not yet paid</i></td>
+        {{ end }}
       </tr>
     {{ end }}
-    {{ if gt (len .Purchases ) 1 }}
-      <tr>
-        {{ if .MultipleBidders }}
-	  <td></td>
-	{{ end }}
-        <td style="font-weight:bold;text-align:right">TOTAL</td>
-	<td style="font-weight:bold;text-align:right;border-top:thin solid black">${{ .TotalAmount }}</td>
-	{{ if and .HasValues .ShowTotalValue }}
-	  <td style="font-weight:bold;text-align:right;border-top:thin solid black">${{ .TotalValue }}</td>
-	{{ end }}
-      </tr>
+    {{ if .TotalAmount }}
+      {{ if .ShowTotalValue }}
+        <tr>
+          <td style="font-weight:bold;text-align:right">TOTAL</td>
+  	  <td style="font-weight:bold;text-align:right;border-top:thin solid black">${{ .TotalValue }}</td>
+  	  <td style="font-weight:bold;text-align:right;border-top:thin solid black">${{ .TotalAmount }}</td>
+        </tr>
+      {{ else }}
+        <tr>
+          <td colspan="2" style="font-weight:bold;text-align:right">TOTAL</td>
+  	  <td style="font-weight:bold;text-align:right;border-top:thin solid black">${{ .TotalAmount }}</td>
+        </tr>
+      {{ end }}
     {{ end }}
   </tbody>
 </table>
-`
-var pledgeTableTemplate = `
-<table>
-  <thead>
-    <tr>
-      <th style="text-align:left">Item</th>
-      {{ if .MultipleBidders }}
-	<th style="text-align:left;padding-left:1em">Bidder</th>
-      {{ end }}
-      <th style="text-align:right;padding-left:1em">Amount Pledged</th>
-      {{ if .HasValues }}
-        <th style="text-align:right;padding-left:1em">Estimated Value</th>
-      {{ end }}
-    </tr>
-  </thead>
-  <tbody>
-    {{ range .Pledges }}
-      <tr>
-        <td>{{ .Item }}</td>
-	{{ if $.MultipleBidders }}
-	  <td style="padding-left:1em">{{ .Bidder }}</td>
-	{{ end }}
-	<td style="text-align:right">{{ .Amount }}</td>
-	{{ if $.HasValues }}
-	  <td style="text-align:right">{{ .Value }}</td>
-	{{ end }}
-      </tr>
-    {{ end }}
-  </tbody>
-</table>
-`
-var payerTemplate = template.Must(template.New("payer").Parse(`
-<div class="receipt">
-{{ if .Purchases }}
-<p>Schola Cantorum confirms the following purchases and donations from {{ .Payer }}{{ if .Date }}, paid on {{ .Date }} by {{ .Method }}{{ end }}:</p>
-{{ template "paidTable" . }}
+{{ if .ShowRegistrationNote }}
+  <div><sup>*</sup> The "estimated value received" is our good faith estimate.  It may not reflect the fair market value.</div>
 {{ end }}
-{{ if .Pledges }}
-<p>Schola Cantorum acknowledges the following pledges from {{ .Payer }}, which we look forward to receiving:</p>
-{{ template "pledgeTable" . }}
+{{ if .ShowPurchaseNote }}
+  <div><sup>†</sup> The "estimated value received" was provided by the donor.  It may not reflect the fair market value.</div>
+{{ end }}
+{{ if .ShowDonationNote }}
+  <div><sup>§</sup> No goods or services were received in return for this donation.</div>
 {{ end }}
 <p>
   Schola Cantorum is a 501(c)(3) tax-exempt organization.
   Our federal tax ID number is 94‑2597822.
-  {{ if not .HasValues }}
-    No goods or services were provided in return for your donation.
-  {{ end }}
 </p>
 <p>Thank you for your support of Schola Cantorum!</p>
 </div>
 `))
-
-func init() {
-	template.Must(payerTemplate.New("paidTable").Parse(paidTableTemplate))
-	template.Must(payerTemplate.New("pledgeTable").Parse(pledgeTableTemplate))
-}
